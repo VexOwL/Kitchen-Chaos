93a70be542b6ea42a0834875be54864d2a5373e3
VexOwL <nickita.vex@gmail.com>
2024-03-10 21:08:20 +0300

    Clear counter, interaction
    Clear counter prefab and interaction handler started

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,51 +3,68 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 7f;
-    [SerializeField] private GameInput gameInput;
-    public bool IsWalking {get; private set;}
-    private float _rotateSpeed = 10f, _playerRadius = 0.7f, _playerHeight = 2, moveDistance;
-    private bool canMove = true;
-    private Vector2 inputVector;
-    private Vector3 moveDirection, moveDirX, moveDirZ;
+    [SerializeField] private GameInput _gameInput;
+    [SerializeField] private LayerMask _countersLayerMask;
+    private float _rotateSpeed = 10f, _playerRadius = 0.7f, _playerHeight = 2, _interactDistance = 2f, _moveDistance;
+    private bool _canMove = true;
+    private Vector2 _inputVector;
+    private Vector3 _moveDirection, _moveDirX, _moveDirZ;
+    //private Vector3 _lookDirection;
+    public bool IsWalking { get; private set; }
 
     private void FixedUpdate()
     {
-        inputVector = gameInput.GetMovementVectorNormalized();
-        moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
-        moveDirX = new Vector3(moveDirection.x, 0, 0);
-        moveDirZ = new Vector3(0, 0, moveDirection.z);
-        moveDistance = _moveSpeed * Time.fixedDeltaTime;
+        HandleMovement();
+        HandleInteractions();
+    }
 
-        canMove = IsCanMove(moveDirection);
-
-        if(!canMove)
+    private void HandleInteractions()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out RaycastHit raycastHit, _interactDistance, _countersLayerMask))
         {
-            canMove = IsCanMove(moveDirX);
+            if(raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+                clearCounter.Interact();
+        }
+    }
 
-            if(canMove)
-                moveDirection = moveDirX;
+    private void HandleMovement()
+    {
+        _inputVector = _gameInput.GetMovementVectorNormalized();
+        _moveDirection = new Vector3(_inputVector.x, 0, _inputVector.y);
+        _moveDirX = new Vector3(_moveDirection.x, 0, 0);
+        _moveDirZ = new Vector3(0, 0, _moveDirection.z);
+        _moveDistance = _moveSpeed * Time.fixedDeltaTime;
+
+        _canMove = IsCanMove(_moveDirection);
+
+        if (!_canMove)
+        {
+            _canMove = IsCanMove(_moveDirX);
+
+            if (_canMove)
+                _moveDirection = _moveDirX;
             else
             {
-                canMove = IsCanMove(moveDirZ);
+                _canMove = IsCanMove(_moveDirZ);
 
-                if(canMove)
-                    moveDirection = moveDirZ;
+                if (_canMove)
+                    _moveDirection = _moveDirZ;
             }
         }
-        
-        if (canMove)
+
+        if (_canMove)
         {
-            transform.position += moveDirection * moveDistance;
+            transform.position += _moveDirection * _moveDistance;
         }
 
-        IsWalking = moveDirection != Vector3.zero;
+        IsWalking = _moveDirection != Vector3.zero;
 
-        transform.forward = Vector3.Slerp(transform.forward, moveDirection, _rotateSpeed * Time.fixedDeltaTime);
+        transform.forward = Vector3.Slerp(transform.forward, _moveDirection, _rotateSpeed * Time.fixedDeltaTime);
     }
 
     private bool IsCanMove(Vector3 moveDirection)
     {
-        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerHeight, _playerRadius, moveDirection, moveDistance);
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * _playerHeight, _playerRadius, moveDirection, _moveDistance);
 
         return canMove;
     }
